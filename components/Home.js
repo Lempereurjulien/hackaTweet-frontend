f03a25e1e97d5724fa89485e864d7c5cd13da044
Camy~ <camyms13@gmail.com>
2023-04-27 16:22:15 +0200

    header vide

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,9 @@
-import styles from '../styles/Home.module.css';
-import Button from '../components/Button';
+import styles from "../styles/Home.module.css";
+import Button from "../components/Button";
 function Home() {
   return (
     <div>
-      <Button text={'Sign up'}></Button>
+      <Button text={"Sign up"}></Button>
     </div>
   );
 }
