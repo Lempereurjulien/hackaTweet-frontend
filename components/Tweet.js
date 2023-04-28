29fea1690c7d182fe2a45fdaeb215c585ac0eecd
Lempereur <lempereur.julien83@gmail.com>
2023-04-28 12:50:57 +0200

    s

--- components/Tweet.js
+++ components/Tweet.js
@@ -5,19 +5,20 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import { useDispatch,useSelector } from 'react-redux';
 import { addHashtag, removeHashtag } from '../reducers/hashtag';
+import { addLike, removeLike } from '../reducers/likes';
 export default function Tweet(props){
 
     const dispatch = useDispatch();
 
     const users = useSelector((state) => state.users.value)
 
+    console.log('users', users);
     const [like, setLike] = useState(false);
     let trashStyle = {'color' : 'white'};
 
     const pattern = /#[a-z]*/gi;
 
     /**AJOUTER HASHTAG DANS REDUCER*/
-
     // const hastag = props.tweet.match(pattern);
     // if(hastag){
     //     dispatch(addHashtag(hastag));
@@ -34,10 +35,13 @@ export default function Tweet(props){
     let heartStyle = {}
     let number = {};
     
+    /**AJOUTER LIKE DANS REDUCER + STYLE */
     if(like){
         heartStyle = {'color' : 'red'};
         number = {'display' : '', 
                 'color' : 'red'}
+        // dispatch(addLike(users))
+        
     } 
     else{
         heartStyle = {'color' : 'white'},
--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
 import users from '../reducers/users';
+import hashtag from '../reducers/hashtag'
+import likes from '../reducers/likes';
 import {persistStore, persistReducer} from 'redux-persist';
 import {PersistGate} from 'redux-persist/integration/react';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers,configureStore } from '@reduxjs/toolkit';
 
-const reducers = combineReducers({users});
+const reducers = combineReducers({users,hashtag, likes});
 const persistConfig = {key : 'hackatweet', storage};
 const store = configureStore({
   reducer: persistReducer(persistConfig, reducers),
